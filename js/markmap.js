78cb501c37bb8e1f48a898e5a2782a7408998113
x2037150585 <2037150585@qq.com>
2024-08-16 14:40:42 +0800

    Site updated: 2024-08-16 14:40:42

--- js/markmap.js
+++ js/markmap.js
@@ -30,7 +30,7 @@
         "d3": "https://fastly.jsdelivr.net/npm/d3@6",
         "markmap_view": "https://fastly.jsdelivr.net/npm/markmap-view@0.2.7",
       },
-      "css": [],
+      "css": ["https://fastly.jsdelivr.net/npm/katex@0.12.0/dist/katex.min.css",],
     }
     const loadElement = (elname, attr) => {
       const el = document.createElement(elname)
